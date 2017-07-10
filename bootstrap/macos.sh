4ddf941ceeae2605f8d8ffa6661576c68ff4e0e9
nullkal <nullkal@nil.nu>
2017-07-10 12:54:25 +0900

    Write bootstrap script for macos

---
+++ bootstrap/macos.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install softwares
+brew install rbenv nvm tmux zsh git neovim
+
+# Write ~/.zshrc
+cat <<'ZSHRC' > ~/.zshrc
+eval "$(rbenv init -)"
+
+export NVM_DIR="$HOME/.nvm"
+. "/usr/local/opt/nvm/nvm.sh"
+
+ZSHRC
