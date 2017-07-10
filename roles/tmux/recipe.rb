5769534666fde0d952db6fd700b8346d896682f7
nullkal <nullkal@nil.nu>
2017-07-10 13:26:40 +0900

    Fix tmux's loading script

--- roles/tmux/recipe.rb
+++ roles/tmux/recipe.rb
@@ -3,5 +3,5 @@
 describe 'Inserting the loading script into .tmux.conf'
 block_in_file File.join(@target_dir, '.tmux.conf'),
   block: <<TMUX_CONF
-for f in `find #{File.join(__dir__, 'tmux.conf.d')} -name '*.tmux' -type f`; do . $f; done
+run-shell 'for f in `find #{File.join(__dir__, 'tmux.conf.d')} -name "*.tmux" -type f`; do . $f; done'
 TMUX_CONF
